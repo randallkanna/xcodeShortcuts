c095c73a0b27882b24a7589728105a9b8f352f29
randallkanna <randallkanna@gmail.com>
2016-02-21 15:58:46 -0800

    modification of app structure

--- xcodeShortcuts/xcodeShortcuts/ViewController.swift
+++ xcodeShortcuts/xcodeShortcuts/ViewController.swift
@@ -12,13 +12,20 @@ class ViewController: UIViewController {
 
     @IBOutlet weak var showShortcutButton: UIButton!
     @IBOutlet weak var shortcutLabel: UILabel!
+    @IBOutlet weak var shortcutDescriptionLabel: UILabel!
     
     let shortcuts = ShortcutList()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        shortcutLabel.text = shortcuts.randomShorcut()
+        let shortcutItem = shortcuts.randomShortcut()
+        
+        shortcutLabel.text = shortcutItem[0]
+        shortcutDescriptionLabel.text = shortcutItem[1]
+        
+//        shortcutLabel.text = shortcuts.randomShorcut()
+//        shortcutDescriptionLabel.text = shortcuts.randomShorcut()
         
         let myColor : UIColor = UIColor( red: 0, green: 0.2235, blue: 0.5373, alpha: 1.0 )
         showShortcutButton.layer.borderColor = myColor.CGColor
@@ -29,9 +36,14 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
     @IBAction func showShortcuts() {
-        shortcutLabel.text = shortcuts.randomShorcut()
+        let shortcutItem = shortcuts.randomShortcut()
+    
+        shortcutLabel.text = shortcutItem[0]
+        shortcutDescriptionLabel.text = shortcutItem[1]
+        
+//        shortcutLabel.text = shortcuts.randomShorcut()
+//        shortcutDescriptionLabel.text = shortcuts.randomShorcut()
     }
 }
 
