16fec02d6894bf1c940ef5cbee1884eba3eb5993
randallkanna <randallkanna@gmail.com>
2016-02-21 20:29:02 -0800

    temporary refresh button

--- xcodeShortcuts/xcodeShortcuts/RandomShortcutViewController.swift
+++ xcodeShortcuts/xcodeShortcuts/RandomShortcutViewController.swift
@@ -25,9 +25,6 @@ class RandomShortcutViewController: UIViewController {
         
         shortcutLabel.text = shortcutItem[0]
         shortcutDescriptionLabel.text = shortcutItem[1]
-        
-        let myColor : UIColor = UIColor( red: 0, green: 0.2235, blue: 0.5373, alpha: 1.0 )
-        showShortcutButton.layer.borderColor = myColor.CGColor
     }
     
     override func didReceiveMemoryWarning() {
